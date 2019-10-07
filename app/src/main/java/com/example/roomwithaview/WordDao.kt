da46daf96235e6e287bb9dcb422a468b5ac752e2
Alexander Saavedra <awsaavedra@gmail.com>
2019-10-06 20:44:40 -0700

    added a room database

--- app/src/main/java/com/example/roomwithaview/WordDao.kt
+++ app/src/main/java/com/example/roomwithaview/WordDao.kt
@@ -1,5 +1,6 @@
 package com.example.roomwithaview
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
@@ -9,7 +10,7 @@ import androidx.room.Query
 interface WordDao {
 
     @Query("SELECT * from word_table ORDER BY word ASC")
-    fun getAllWords(): List<Word>
+    fun getAllWords(): LiveData<List<Word>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(word: Word)
